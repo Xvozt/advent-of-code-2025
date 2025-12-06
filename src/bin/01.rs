d994074c25e3848d49e3742c3f3cc646641c7ed2
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-12-06 18:54:52 +0400

    day 1: part 2 solution

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,6 +3,7 @@ advent_of_code::solution!(1);
 pub fn part_one(input: &str) -> Option<u64> {
     let numbers = input
         .split('\n')
+        .filter(|s| !s.is_empty())
         .map(|s| -> i16 {
             let (direction, number) = (&s[..1], &s[1..]);
             match direction {
@@ -25,8 +26,48 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(count)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let numbers = input
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(|s| -> i16 {
+            let (direction, number) = (&s[..1], &s[1..]);
+            match direction {
+                "L" => -number.to_string().parse::<i16>().unwrap(),
+                "R" => number.to_string().parse::<i16>().unwrap(),
+                _ => unreachable!(),
+            }
+        })
+        .collect::<Vec<i16>>();
+
+    let mut position = 50i16;
+    let mut count = 0u64;
+
+    for &rotation in &numbers {
+        let distance = rotation.abs();
+
+        let full_circles = distance / 100;
+        count += full_circles as u64;
+
+        let reminder = distance % 100;
+
+        if rotation > 0 {
+            if position + reminder > 100 {
+                count += 1;
+            }
+        } else if rotation < 0 {
+            if position != 0 && position - reminder < 0 {
+                count += 1;
+            }
+        }
+
+        position = (position + rotation).rem_euclid(100);
+        if position == 0 {
+            count += 1;
+        }
+    }
+
+    return Some(count);
 }
 
 #[cfg(test)]
@@ -42,6 +83,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
