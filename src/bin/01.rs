d52f5f79fe9d3d31f8306c8e49e3e9a7bce35292
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-12-06 17:31:33 +0400

    day 1: part 2 solution

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,10 +1,31 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let numbers = input
+        .split('\n')
+        .map(|s| -> i16 {
+            let (direction, number) = (&s[..1], &s[1..]);
+            match direction {
+                "L" => -number.to_string().parse::<i16>().unwrap(),
+                "R" => number.to_string().parse::<i16>().unwrap(),
+                _ => unreachable!(),
+            }
+        })
+        .collect::<Vec<i16>>();
+
+    let mut position = 50i16;
+    let mut count = 0u64;
+
+    for &rotation in &numbers {
+        position = (position + rotation).rem_euclid(100);
+        if position == 0 {
+            count += 1;
+        }
+    }
+    Some(count)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
@@ -15,7 +36,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
